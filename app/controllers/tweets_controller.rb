8fcbdf4a31f71fa4d27508ab7c34c88030aebbe5
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-03-09 12:12:19 +0900

    リファクタリング

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,8 +5,6 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
-    #query = "SELECT * FROM tweets"
-    #@tweets = Tweet.find_by_sql(query)
   end
 
   def new
@@ -14,7 +12,6 @@ class TweetsController < ApplicationController
   end
 
   def create
-    # binding.pry
     Tweet.create(tweet_params)
   end
 
@@ -32,41 +29,21 @@ class TweetsController < ApplicationController
   end
 
   def show
-    #@tweet = Tweet.find(params[:id])
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
   end
 
-
-
-
   def search
-  # @tweets = SearchTweetsService.search(params[:keyword])
-   # @tweets = Tweet.search(params[:keyword])
-   
       # キーワード分割
       keywords = params[:keyword].split(/[[:blank:]]+/).select(&:present?)
-     # binding.pry
       # 空のモデルオブジェクト作成（何も入っていない空配列のようなもの）
       @tweets = Tweet.none
 
       # 検索ワードの数だけor検索を行う
       keywords.each do |keyword|
-       # binding.pry
-        #keyword = keyword.tr('〇一二三四五六七八九', '0123456789').gsub(/(\d+)?十(\d+)?/) { ($1 || 1).to_i * 10 + $2.to_i }
         @tweets = @tweets.or(Tweet.where("text LIKE ?", "%#{keyword}%"))
       end
-
-        #YaKansuji.to_kan(keyword)
-        
-        #YaKansuji.to_i("三") # => 3
   end
-
-
-
-
-
-
 
   private
   def tweet_params
